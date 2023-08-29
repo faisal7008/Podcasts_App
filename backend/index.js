a78e8e4861b34b1073b3e917063d3e09d73f14c6
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-08-29 13:43:10 +0530

    update mongodb uri to production

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const connectDB = async () => {
   try {
     await mongoose
-      .connect(process.env.MONGO_URI_LOCAL, {
+      .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
       })
       .then(() => {
